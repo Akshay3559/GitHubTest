6c217fab8bc72f286226e669aed6ae06069bfb68
Akshay3559 <akshaynalawade90@gmail.com>
2022-02-18 17:20:41 +0530

    Second GITHUB Committed.

---
+++ WhileLoop/src/typesofloop/WhileLoop.java
@@ -0,0 +1,30 @@
+package typesofloop;
+
+public class WhileLoop {
+	
+	public static void main(String[] args) {
+		
+		// Write numbers from 1 to 10
+		
+		int i=1;
+		while (i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+
+		// do-while loop
+		// Write numbers from 1 to 10;
+		
+		
+		int j=1;
+		do
+		{
+			System.out.println(j);
+			j++;
+		}
+		while(j<=10);
+	}
+
+}
